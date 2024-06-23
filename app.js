e00cf52c963c6be3ff8097341b5f2a97465235a1
kiran <110989705+kkiran98@users.noreply.github.com>
2024-06-23 11:11:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.post('/add-more', upload.single('image'), async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
